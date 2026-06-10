1c242b40c14d96a10f4b93843ac3cfb42fff1d27
Daniel Uribe <daniel.uribe.garcia07@gmail.com>
2026-06-10 01:27:43 -0600

    fix: supabase import paths

--- src/components/dibujo/DibujoPlayControls.tsx
+++ src/components/dibujo/DibujoPlayControls.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect, useRef } from "react";
-import { supabase } from "../../supabase";
+import { supabase } from "../../lib/supabase";
 import { FaPaintBrush, FaEraser, FaTrash, FaPaperPlane } from "react-icons/fa";
 
 export default function DibujoPlayControls() {
